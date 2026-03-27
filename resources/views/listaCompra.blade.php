e03a251ce09fadf281d73957edeabb9ce15df279
adrimorato47 <adsagil@gmail.com>
2026-03-27 13:53:27 +0100

    crea la tabla tareas y el modelo tengo que ver si esta bien

--- resources/views/listaCompra.blade.php
+++ resources/views/listaCompra.blade.php
@@ -39,6 +39,7 @@
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                 <h1>Aqui va ista de la compra</h1>
+                
             </main>
         </div>
 
